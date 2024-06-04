d341feeadc907cc5a7b52af8ff90a0a4515117e0
Nils <nilshoeller00@gmail.com>
2024-06-04 09:07:20 +0200

    react router in navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class Navigationbar extends Component {
   render() {
@@ -9,10 +10,18 @@ class Navigationbar extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/text-analyse">Text Analyse</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/new-account">New Account</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/text-analyse">
+                Text Analyse
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/new-account">
+                New Account
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
