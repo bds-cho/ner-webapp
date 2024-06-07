2db5cfd1d768c05d4805ffa5aaacaefe83d794d2
= <=>
2024-06-07 22:35:27 +0200

    refator class component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { useGetSession } from "../api/authentication";
+export function Navigationbar() {
+  const { data } = useGetSession();
 
-class Navigationbar extends Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="lg" className="custom-rounded-navbar">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/text-analyse">
-                Text Analyse
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={Link} to="/new-account">
-                New Account
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar bg="light" expand="lg" className="custom-rounded-navbar">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/text-analyse">
+              Text Analyse
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/new-account">
+              New Account
+            </Nav.Link>
+          </Nav>
+          {data?.isAuthenticated && <span>Eingeloggt als {data.username}</span>}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigationbar;
