d7d19d89941be03d75d5f1f6411f2bd1d2a98047
Nils <nilshoeller00@gmail.com>
2024-06-17 08:29:24 +0200

    changed style of feed items

--- frontend/src/components/FeedTextItem.jsx
+++ frontend/src/components/FeedTextItem.jsx
@@ -1,18 +1,43 @@
+// import React from "react";
+// import { Card } from "react-bootstrap";
+// import "../styles/FeedTextItem.css";
+
+// const FeedTextItem = ({ user, text }) => {
+//   return (
+//     <Card className="mb-3 text-item-card">
+//       <Card.Body>
+//         <Card.Title>
+//           <h2>
+//             {user.first_name} {user.last_name}
+//           </h2>
+//           <h5>{user.username}</h5>
+//         </Card.Title>
+//         <Card.Text>{text}</Card.Text>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
+
+// export default FeedTextItem;
+
 import React from "react";
 import { Card } from "react-bootstrap";
-import "../styles/TextItem.css";
+import "../styles/FeedTextItem.css";
 
 const FeedTextItem = ({ user, text }) => {
   return (
     <Card className="mb-3 text-item-card">
       <Card.Body>
-        <Card.Title>
-          <h2>
-            {user.first_name} {user.last_name}
-          </h2>
-          <h5>{user.username}</h5>
-        </Card.Title>
-        <Card.Text>{text}</Card.Text>
+        <div className="text-item-header">
+          <div className="user-info">
+            <h2 className="user-name">
+              {user.first_name} {user.last_name}
+            </h2>
+            <h5 className="user-username">{user.username}</h5>
+          </div>
+        </div>
+        <hr className="divider" />
+        <Card.Text className="text-content">{text}</Card.Text>
       </Card.Body>
     </Card>
   );
