acec2716e7c26c7891fcbf04a0d23777ca18851c
Bhupendra Singh <b.singh@campus.tu-berlin.de>
2024-07-22 01:03:50 +0200

    switch to postgres

--- backend/sksy_backend/settings.py
+++ backend/sksy_backend/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = "sksy_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maindb',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
